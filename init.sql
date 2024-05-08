2f7afc37ddb845f32222029357cbe4cf46c0f9c3
Dereto <nobody7548@gmail.com>
2024-05-08 16:34:14 +0800

    fix init.sql

--- init.sql
+++ init.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS users (
 
 
 -- Insert data
-INSERT INTO users (usernameu, password, point) VALUES
+INSERT INTO users (username, password, point) VALUES
     ('admin', '$2b$12$s1wj98ds1os1AtIOoBHAT.0h1JlFsm/Htg5Es30PJQYmYjDALHZWK', 999),
     ('Alice', '$2b$12$xUbazOYRgTZ5XD9fajfM1uXvLmtAI5.nYsfmunp3aipppYsuW9vlC', 10),
     ('Bob', '$2b$12$xUbazOYRgTZ5XD9fajfM1uXvLmtAI5.nYsfmunp3aipppYsuW9vlC', 50);
